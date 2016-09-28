01b676ed6e25175faf1181c86cd309430d9d7569
Kostiantyn Minkov <Kostiantyn_Minkov@epam.com>
2016-09-28 12:54:56 +0300

    add eatTest

--- src/main/java/HealthService.java
+++ src/main/java/HealthService.java
@@ -1,5 +1,30 @@
-/**
- * Created by Kostiantyn_Minkov on 9/28/2016.
- */
+
+
 public class HealthService {
+
+    private final double WATER_DAY_NORM;
+    private final int CALORIES_DAY_NORM;
+    private double waterAmountDrunk;
+    private int caloriesAmountAte;
+
+    public HealthService(double waterDayNorm, int caloriesDayNorm) {
+        WATER_DAY_NORM = waterDayNorm;
+        CALORIES_DAY_NORM = caloriesDayNorm;
+    }
+
+    public void drink(double amount) {
+        waterAmountDrunk += amount;
+    }
+
+    public double waterLeftToDrink() {
+        return WATER_DAY_NORM - waterAmountDrunk;
+    }
+
+    public void eat(int calories) {
+        caloriesAmountAte += calories;
+    }
+
+    public int caloriesLeftToEat() {
+        return CALORIES_DAY_NORM - caloriesAmountAte;
+    }
 }
--- src/test/java/ClassForTestTest.java
+++ src/test/java/ClassForTestTest.java
@@ -1,22 +1,34 @@
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
 
-/**
- * Created by Kostiantyn_Minkov on 9/28/2016.
- */
+
 public class ClassForTestTest {
 
     private static HealthService healthService;
+    private static final double WATER_DAY_NORM = 2.5;
+    private static final int CALORIES_DAY_NORM = 2200;
 
     @BeforeClass
     public static void before() throws Exception {
-        healthService = new HealthService();
+        healthService = new HealthService(WATER_DAY_NORM, CALORIES_DAY_NORM);
     }
 
+    @Test
+    public void drinkTest() throws Exception {
+        double drunkAmount = .200;
+        double waterLeft = WATER_DAY_NORM - drunkAmount;
+        healthService.drink(drunkAmount);
+        assertThat(healthService.waterLeftToDrink(), is(waterLeft));
+    }
+
+    @Test
+    public void eatTest() throws Exception {
+        int caloriesAmount = 500;
+        int caloriesLeft = CALORIES_DAY_NORM - caloriesAmount;
+        healthService.eat(caloriesAmount);
+        assertThat(healthService.caloriesLeftToEat(), is(caloriesLeft));
+    }
 }
