870a189720ca8c6eb21adb2c9e02551a442b486d
Kostiantyn Minkov <Kostiantyn_Minkov@epam.com>
2016-09-28 12:14:47 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Kostiantyn_Minkov on 9/28/2016.
+ */
+public class Main {
+}
---
+++ src/test/java/ClassForTestTest.java
@@ -0,0 +1,25 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Kostiantyn_Minkov on 9/28/2016.
+ */
+public class ClassForTestTest {
+
+    private static HealthService healthService;
+
+    @BeforeClass
+    public static void before() throws Exception {
+        healthService = new HealthService();
+    }
+
+    @Test
+    public
+
+}
