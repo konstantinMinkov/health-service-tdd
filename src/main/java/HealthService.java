784a73c0a42bb22ba2f454245d1d755aaed9cbe6
Kostiantyn Minkov <Kostiantyn_Minkov@epam.com>
2016-09-30 12:30:33 +0300

    add LocalDateTime support

--- src/main/java/HealthService.java
+++ src/main/java/HealthService.java
@@ -1,30 +1,70 @@
-
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class HealthService {
 
     private final double WATER_DAY_NORM;
     private final int CALORIES_DAY_NORM;
-    private double waterAmountDrunk;
-    private int caloriesAmountAte;
+    private final int WALKING_MINUTES_DAY_NORM;
+    private Map<LocalDate, Optional<Double>> waterAmountDrunk;
+    private Map<LocalDate, Optional<Integer>> caloriesAmountAte;
+    private Map<LocalDate, Optional<Integer>> minutesWalked;
 
-    public HealthService(double waterDayNorm, int caloriesDayNorm) {
+    public HealthService(double waterDayNorm, int caloriesDayNorm,
+                         int walkingMinutesDayNorm) {
         WATER_DAY_NORM = waterDayNorm;
         CALORIES_DAY_NORM = caloriesDayNorm;
+        WALKING_MINUTES_DAY_NORM = walkingMinutesDayNorm;
+        caloriesAmountAte = new HashMap<>();
+        waterAmountDrunk = new HashMap<>();
+        minutesWalked = new HashMap<>();
     }
 
-    public void drink(double amount) {
-        waterAmountDrunk += amount;
+    public void eat(LocalDateTime dateTime, int calories) {
+        int totalCalories = calories;
+        final LocalDate date = dateTime.toLocalDate();
+        if (caloriesAmountAte.containsKey(date)) {
+            totalCalories += caloriesAmountAte.get(date).orElse(0);
+        }
+        caloriesAmountAte.put(date, Optional.ofNullable(totalCalories));
     }
 
-    public double waterLeftToDrink() {
-        return WATER_DAY_NORM - waterAmountDrunk;
+    public void drink(LocalDateTime dateTime, double amount) {
+        double totalAmount = amount;
+        final LocalDate date = dateTime.toLocalDate();
+        if (waterAmountDrunk.containsKey(date)) {
+            totalAmount += waterAmountDrunk.get(date).orElse(.0);
+        }
+        waterAmountDrunk.put(date, Optional.ofNullable(totalAmount));
     }
 
-    public void eat(int calories) {
-        caloriesAmountAte += calories;
+    public void walk(LocalDateTime dateTime, int minutesToWalk) {
+        int totalMinutes = minutesToWalk;
+        final LocalDate date = dateTime.toLocalDate();
+        if (minutesWalked.containsKey(date)) {
+            totalMinutes += minutesWalked.get(date).orElse(0);
+        }
+        minutesWalked.put(date, Optional.ofNullable(totalMinutes));
     }
 
-    public int caloriesLeftToEat() {
-        return CALORIES_DAY_NORM - caloriesAmountAte;
+    public double waterLeftToDrink(LocalDateTime dateTime) {
+        final LocalDate date = dateTime.toLocalDate();
+        return WATER_DAY_NORM - waterAmountDrunk.get(date).orElse(.0);
     }
+
+    public int caloriesLeftToEat(LocalDateTime dateTime) {
+        final LocalDate date = dateTime.toLocalDate();
+        return CALORIES_DAY_NORM - caloriesAmountAte.get(date).orElse(0);
+    }
+
+    public int minutesLeftToWalk(LocalDateTime dateTime) {
+        final LocalDate date = dateTime.toLocalDate();
+        return WALKING_MINUTES_DAY_NORM - minutesWalked.get(date).orElse(0);
+    }
+
+
 }
--- src/test/java/HealthServiceTest.java
+++ src/test/java/HealthServiceTest.java
@@ -1,34 +1,46 @@
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 
-public class ClassForTestTest {
+public class HealthServiceTest {
 
     private static HealthService healthService;
     private static final double WATER_DAY_NORM = 2.5;
     private static final int CALORIES_DAY_NORM = 2200;
+    private static final int WALKING_MINUTES_DAY_NORM = 120;
 
     @BeforeClass
     public static void before() throws Exception {
-        healthService = new HealthService(WATER_DAY_NORM, CALORIES_DAY_NORM);
+        healthService = new HealthService(WATER_DAY_NORM, CALORIES_DAY_NORM,
+                WALKING_MINUTES_DAY_NORM);
     }
 
     @Test
     public void drinkTest() throws Exception {
         double drunkAmount = .200;
         double waterLeft = WATER_DAY_NORM - drunkAmount;
-        healthService.drink(drunkAmount);
-        assertThat(healthService.waterLeftToDrink(), is(waterLeft));
+        healthService.drink(LocalDateTime.now(), drunkAmount);
+        assertThat(healthService.waterLeftToDrink(LocalDateTime.now()), is(waterLeft));
     }
 
     @Test
     public void eatTest() throws Exception {
         int caloriesAmount = 500;
         int caloriesLeft = CALORIES_DAY_NORM - caloriesAmount;
-        healthService.eat(caloriesAmount);
-        assertThat(healthService.caloriesLeftToEat(), is(caloriesLeft));
+        healthService.eat(LocalDateTime.now(), caloriesAmount);
+        assertThat(healthService.caloriesLeftToEat(LocalDateTime.now()), is(caloriesLeft));
+    }
+
+    @Test
+    public void walkTest() throws Exception {
+        int minutesWalked = 20;
+        int minutesToWalk = WALKING_MINUTES_DAY_NORM - minutesWalked;
+        healthService.walk(LocalDateTime.now(), minutesWalked);
+        assertThat(healthService.minutesLeftToWalk(LocalDateTime.now()), is(minutesToWalk));
     }
 }
